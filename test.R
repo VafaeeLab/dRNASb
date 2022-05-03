ffbebc1eefe688ac894b192e9c103de254541b15
abhivij <abhishek.kcvijayan@gmail.com>
2022-05-03 23:14:11 +1000

    R package creation

--- test.R
+++ test.R
@@ -1,1 +1,17 @@
-print("test file")
+# devtools::create("~/UNSW/VafaeeLab/HostPathogenInteraction/dRNASb")
+
+# BiocManager::install("Mfuzz")
+# install.packages("ClueR")
+
+setwd("~/UNSW/VafaeeLab/HostPathogenInteraction/dRNASb")
+
+devtools::document()
+devtools::install()
+
+
+
+# result <- dRNAR::seq_analyze("Alldat.csv", "pheno.csv", "GO_Function.csv")
+#
+# hou2_result <- result[[1]]
+dRNASb::get_package_info("Abhishek")
+
