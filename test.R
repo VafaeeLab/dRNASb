f0b7d2ea0c6d214736bd8b0ef92186b0b70e5a71
abhivij <abhishek.kcvijayan@gmail.com>
2022-05-04 14:41:00 +1000

    processing data

--- test.R
+++ test.R
@@ -15,3 +15,17 @@ devtools::install()
 # hou2_result <- result[[1]]
 dRNASb::get_package_info("Abhishek")
 
+
+
+dat <- read.csv("Data/Alldat.csv",row.names = 1)
+pheno <- read.csv("Data/pheno.csv", row.names = 1)
+fun<-read.csv("Data/GO_Function.csv")
+ppi<-read.csv("Data/PPI.csv")
+ann<-read.csv("Data/Annotation.csv")
+
+
+data_file_path = "Data/Alldat.csv"
+phenotype_file_path = "Data/pheno.csv"
+go_function_file_path = "Data/GO_Function.csv"
+ppi_file_path = "Data/PPI.csv"
+annotation_file_path = "Data/Annotation.csv"
